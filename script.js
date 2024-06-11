842dc7ed40d251734ae57ab9340be5937d9def8c
velibor918 <velibor918@gmail.com>
2024-06-11 20:54:22 +0200

    Added three basic vars and button qSelectors

---
+++ script.js
@@ -0,0 +1,17 @@
+let varOne = '';
+let varTwo = '';
+let result = '';
+
+let btnOne = document.querySelector('.one');
+let btnTwo = document.querySelector('.two');
+let btnThree = document.querySelector('.three');
+let btnFour = document.querySelector('.four');
+let btnFive = document.querySelector('.five');
+let btnSix = document.querySelector('.six');
+let btnSeven = document.querySelector('.seven');
+let btnEight = document.querySelector('.eight');
+let btnNine = document.querySelector('.nine');
+let btnPlus = document.querySelector('.plus');
+let btnMinus = document.querySelector('.minus');
+let btnDivide = document.querySelector('.divide');
+let btnClear = document.querySelector('.clear');
